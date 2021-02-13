41c575df9285daefb691514a9faf1da8d53610b7
Darshan-Dhameliya <iamdhameliya@gmail.com>
2021-02-13 17:40:32 +0530

    secondcommit

---
+++ src/notes.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+export default function Notes() {
+  const [notes, setNotes] = useState([]);
+  const [getNote, setgetNotes] = useState("");
+
+  function getNotes() {
+    var storednotes = JSON.parse(localStorage.getItem("notes"));
+
+    storednotes !== null && setNotes(storednotes);
+  }
+
+  function addNote(e) {
+    e.preventDefault();
+    if (getNote !== null && getNote.trim().length !== 0) {
+      var newnotes = [...notes, getNote];
+      setNotes(newnotes);
+      localStorage.setItem("notes", JSON.stringify(newnotes));
+      setgetNotes("");
+    }
+  }
+
+  const Deletenote = (noteid) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this notes"
+    );
+    if (confirm) {
+      var removenote = notes.filter((item, index) => index !== noteid);
+      setNotes(removenote);
+      localStorage.setItem("notes", JSON.stringify(removenote));
+    }
+  };
+
+  const editNote = (notedescription, notindex) => {
+    var person = prompt("Please enter your name", notedescription.trim());
+    if (person !== null && person.trim().length !== 0) {
+      var arr = [];
+      for (var i = 0; i < notes.length; i++) {
+        if (i === notindex) {
+          notes[i] = person;
+          setNotes(notes);
+        }
+        arr.push(notes[i]);
+      }
+      setNotes(arr);
+      localStorage.setItem("notes", JSON.stringify(arr));
+    }
+  };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-info text-center m-5 ">Notes</h1>
+      <div className="container">
+        <div className="form-group mt-5">
+          <form onSubmit={(e) => addNote(e)}>
+            <div className="d-flex">
+              <input
+                type="text"
+                value={getNote}
+                className="form-control"
+                onChange={(e) => setgetNotes(e.target.value)}
+              />
+              <button type="submit" className="btn btn-outline-primary ml-2">
+                ADD
+              </button>
+            </div>
+          </form>
+          <div className="mt-3">
+            {notes.map((note, index) => (
+              <div key={index} className="mt-1 d-flex">
+                <span> {index + 1}</span>
+                <input
+                  type="text"
+                  value={note}
+                  disabled
+                  className="form-control"
+                />
+                <button
+                  onClick={() => editNote(note, index)}
+                  className="btn btn-outline-primary ml-2"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => Deletenote(index)}
+                  className="btn btn-outline-danger ml-2"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
